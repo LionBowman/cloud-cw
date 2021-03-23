76145a2893786c779db4a1487f8ff513f71273d7
Leandro <bigbruv44@msn.com>
2021-03-23 17:28:59 +0000

    Refactored the mongoose api calls for use in the app.js file

--- nodejs/src/app.js
+++ nodejs/src/app.js
@@ -1,5 +1,24 @@
+//Express web service library
+const express = require('express')
 const broadcast = requires('./broadcast.js');
 const mongo = requires('./mongo.js');
 
-mongo.getAll();
-mongo.postAll();
+//instance of express and port to use for inbound connections.
+const app = express()
+const port = 3000
+
+//tell express to use the body parser. Note - This function was built into express but then moved to a seperate package.
+app.use(express.json());
+
+app.get('/', (_, res) => {
+    res.send(mongo.getAll());
+})
+  
+app.post('/', (req, res) => {
+    res.send(mongo.postAll(req.body));
+})
+
+//bind the express web service to the port specified
+app.listen(port, () => {
+    console.log(`Express Application listening at port ` + port)
+   })
--- nodejs/src/mongo.js
+++ nodejs/src/mongo.js
@@ -2,30 +2,8 @@
 //Object data modelling library for mongo
 const mongoose = require('mongoose');
 
-//Mongo db client library
-//const MongoClient  = require('mongodb');
-
-//Express web service library
-const express = require('express')
-
-//used to parse the server response from json to object.
-const bodyParser = require('body-parser');
-
-//instance of express and port to use for inbound connections.
-const app = express()
-const port = 3000
-
 //connection string listing the mongo servers. This is an alternative to using a load balancer. THIS SHOULD BE DISCUSSED IN YOUR ASSIGNMENT.
 const connectionString = 'mongodb://localmongo1:27017,localmongo2:27017,localmongo3:27017/notFlixDB?replicaSet=rs0';
-
-setInterval(function() {
-
-  console.log(`Intervals are used to fire a function for the lifetime of an application.`);
-
-}, 3000);
-
-//tell express to use the body parser. Note - This function was built into express but then moved to a seperate package.
-app.use(bodyParser.json());
 
 //connect to the cluster
 mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true});
@@ -49,28 +27,19 @@ var analyticsSchema = new Schema({
 var analyticsModel = mongoose.model('analytics', analyticsSchema, 'analytics');
 
 
-function getAll() {
-  app.get('/', (_, res) => {
-    analyticsModel.find({}, 'accountId userName titleId userAction dateAndTime pointOfInteraction typeOfInteraction', (err, analytics) => {
-      if(err) return handleError(err);
-      res.send(JSON.stringify(analytics))
+function getAll() {  
+  analyticsModel.find({}, 'accountId userName titleId userAction dateAndTime pointOfInteraction typeOfInteraction', (err, analytics) => {
+      if(err) return err;
+      return JSON.stringify(analytics)
     }) 
-  })
-}
-function postAll() {
-  app.post('/', (req, res) => {
-    var new_analytics_instance = new analyticsModel(req.body);
-    new_analytics_instance.save(function (err) {
-    if (err) res.send(err);
-    res.send(JSON.stringify(req.body))
-    });
-   })
 }
 
-
-//bind the express web service to the port specified
-app.listen(port, () => {
- console.log(`Express Application listening at port ` + port)
-})
+function postAll(model) {
+  var new_analytics_instance = new analyticsModel(model);
+  new_analytics_instance.save(function (err) {
+  if (err) return err;
+  return JSON.stringify(new_analytics_instance)
+  });
+}
 
 module.exports = {getAll, postAll};
