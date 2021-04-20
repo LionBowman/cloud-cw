44aad5ef10126e443e710bcbb5f5d7f0da01eda2
Leandro <bigbruv44@msn.com>
2021-04-20 11:42:29 +0100

    Leader election changes.

--- nodejs/src/broadcast.js
+++ nodejs/src/broadcast.js
@@ -19,129 +19,119 @@ var ipaddr = require('dns').lookup(require('os').hostname(), function (err, add,
 })
 
 // Initialise not as the leader
-var systemLeader = 0
+var systemLeader = 0;
+
+// Empty array
+var nodeArr = [];
 
 var nodeID = Math.floor(Math.random() * (100 - 1 + 1) + 1);
 //toSend = {"hostname" : myhostname, "status": "alive","nodeID":nodeID} ;
 
 // Check if leader
-setInterval(function() {
-  console.log(JSON.stringify(nodes));
+function LeaderElection () {
+  console.log(nodeArr);
+  var thisNode = getNode();
   leader = 1;
   activeNodes = 0;
-  Object.entries(nodes).forEach(([hostname,prop]) => {
-    console.log(nodeID) // test
-    console.log("test" + JSON.stringify(hostname) + JSON.stringify(prop) )
-    maxNodeID = nodeID;
-    if(hostname != myhostname){
-      if("nodeID" in prop){
+  nodeArr.forEach((node) => {
+    console.log("test " , node)
+    maxNodeID = thisNode.id;
+    if (node.hostname != thisNode.hostname) {
+      // if ("nodeID" in prop) {
         activeNodes++;
-        if(prop.nodeID > nodeID)
-        {
+        if (node.id > thisNode.id) {
           leader = 0;
         }
-      }
+      //}
     }
-    if((leader == 1) && (activeNodes == (nodes.length - 1)))
-    {
+    if ((leader == 1) && (activeNodes == (nodeArr.length - 1))) {
       systemLeader = 1;
     }
   });
-}, 2000);
+};
 
+function getNode () {
+  const node = {
+    id: nodeID,
+    hostname: os.hostname()
+  };
 
-// Create or append a file with the node names
-// function AddNodeToFile(nodeName) {
-//   fs.appendFile('nodeNames.txt', nodeName, function (err) {
-//     if (err) throw err;
-//     console.log('nodeNames added to the file');
-//   });
-// }
+  return node
+}
 
-// Check if the node name exists
-// if(!nodeNameFileChecked) {
-//   fs.access('nodeNames.txt', (err) => {
-//     if (err) {
-//         console.log("The 'nodeNames.txt' file does not exist.");
-//         AddNodeToFile("");
-//         console.log("An empty 'nodeNames.txt' file has now been created.");
-//         nodeNameFileChecked = true;
-//     } else {
-//         console.log("The file already exists.");
-//         nodeNameFileChecked = true;
-//     }
-// });
-// } else {
-//   AddNodeToFile({hName: hostname, addr: ipaddr});
-// }
+function AddToArray(nodeToAdd) {
+  nodeArr.push(nodeToAdd);
+}
 
-amqp.connect(connectionString, function(error0, connection) {});
+amqp.connect(connectionString, function (error0, connection) { });
 
 // Publish
-amqp.connect(connectionString, function(error0, connection) {
-      if (error0) {
-              throw error0;
-            }
-      console.log("we have a connection using: " + connectionString + "for " + myhostname)
-      connection.createChannel(function(error1, channel) {});
+amqp.connect(connectionString, function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  console.log("we have a connection using: " + connectionString + "for " + myhostname)
+  connection.createChannel(function (error1, channel) { });
 });
 
-  amqp.connect(connectionString, function(error0, connection) {
+amqp.connect(connectionString, function (error0, connection) {
 
-    if (error0) {
-            throw error0;
-          }
-          setInterval(function() {
-          connection.createChannel(function(error1, channel) {
-                  if (error1) {
-                            throw error1;
-                          }
-                  var exchange = 'logs';
-                  var msg =  '{ ' + myhostname + ' }" : "{ ' + defineAddr + ' }"';
-    
-                  channel.assertExchange(exchange, 'fanout', {
-                            durable: false
-                          });
-                  channel.publish(exchange, '', Buffer.from(msg));
-                  console.log(" [x] Sent %s", msg);
-                });
-                
-        setTimeout(function() {
-                  }, 500);
-        }, 1000)
+  if (error0) {
+    throw error0;
+  }
+  setInterval(function () {
+    connection.createChannel(function (error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+      var exchange = 'logs';
+      var msg = JSON.stringify(getNode());
+
+      channel.assertExchange(exchange, 'fanout', {
+        durable: false
+      });
+      channel.publish(exchange, '', Buffer.from(msg));
+      console.log(" [x] Sent %s", msg);
     });
 
+    setTimeout(function () {
+    }, 500);
+  }, 1000)
+});
+
 // Subscribe
-amqp.connect(connectionString, function(error0, connection) {
-      if (error0) {
-              throw error0;
-            }
-      connection.createChannel(function(error1, channel) {
-              if (error1) {
-                        throw error1;
-                      }
-              var exchange = 'logs';
+amqp.connect(connectionString, function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+    var exchange = 'logs';
 
-              channel.assertExchange(exchange, 'fanout', {
-                        durable: false
-                      });
+    channel.assertExchange(exchange, 'fanout', {
+      durable: false
+    });
 
-              channel.assertQueue('', {
-                        exclusive: true
-                      }, function(error2, q) {
-                                if (error2) {
-                                            throw error2;
-                                          }
-                                console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
-                                channel.bindQueue(q.queue, exchange, '');
+    channel.assertQueue('', {
+      exclusive: true
+    }, function (error2, q) {
+      if (error2) {
+        throw error2;
+      }
+      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+      channel.bindQueue(q.queue, exchange, '');
 
-                                channel.consume(q.queue, function(msg) {
-                                            if(msg.content) {
-                                                            console.log(" [x] %s", msg.content.toString());
-                                                          }
-                                          }, {
-                                                      noAck: true
-                                                    });
-                              });
-            });
+      channel.consume(q.queue, function (msg) {
+        if (msg.content) {
+          console.log(" [x] Received %s", msg.content.toString());
+          AddToArray(JSON.parse(msg.content.toString()));
+          LeaderElection();
+        }
+      }, {
+        noAck: true
+      });
+    });
+  });
 });
